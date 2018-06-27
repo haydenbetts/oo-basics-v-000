812d41a0bc9657c35ff45451d4faebf8dcec6e93
Hayden Betts <haydenbetts@gmail.com>
2018-06-27 18:48:02 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,11 @@
 class Shoe
+  attr_accessor :brand, :color, :size, :material, :condition
 
-  def initialize(title)
+  def initialize(brand)
+    @brand = brand
+  end
+
+  def cobble
   end
 
 end
